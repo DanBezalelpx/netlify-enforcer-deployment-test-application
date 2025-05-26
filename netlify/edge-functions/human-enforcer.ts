6f20f0da5e1910e4dd60c7f9f566cb6b93feadf7
enforcers-unihub-application <unihub@humansecurity.com>
2025-05-26 15:08:15 +0300

    testing version

--- netlify/edge-functions/human-enforcer.ts
+++ netlify/edge-functions/human-enforcer.ts
@@ -1,11 +1,24 @@
-import { PXEnforcer, PXRawConfig } from "perimeterx-node-core-ts";
-import type { Config, Context } from "@netlify/edge-functions";
+import { LoggerSeverity, ModuleMode, PXEnforcer, PXRawConfig } from '@chen.zimmer.perimeterx/internal-human-security-netlify-enforcer';
+import type { Context } from "@netlify/edge-functions";
 
 export default async function handler(request: Request, context: Context) {
   const config: PXRawConfig = {
     px_app_id: Netlify.env.get("PX_APP_ID"),
     px_cookie_secret: Netlify.env.get("PX_COOKIE_SECRET"),
     px_auth_token: Netlify.env.get("PX_AUTH_TOKEN"),
+    px_logger_severity: LoggerSeverity.DEBUG,
+    px_module_mode: ModuleMode.ACTIVE_BLOCKING,
+    px_login_credentials_extraction_enabled: true,
+    px_login_credentials_extraction: [ {
+      path: "/login",
+      path_type: "exact",
+      sent_through: "body",
+      method: "POST",
+      user_field: "username",
+      pass_field: "password",
+      protocol: "v1",
+      login_successful_reporting_method: "header"
+    }],
     px_extract_user_ip: (request) => {
       return context.ip;
     },
